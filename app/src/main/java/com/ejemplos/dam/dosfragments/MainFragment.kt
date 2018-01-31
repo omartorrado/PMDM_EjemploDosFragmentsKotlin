bad363842455dbdaa009a6f1d2e0f01d66b99e65
Damian <damian@danielcastelao.org>
2018-01-31 07:51:02 +0100

    Fragment y su layout. Añadimos Anko (https://github.com/Kotlin/anko)

--- app/src/main/java/com/ejemplos/dam/dosfragments/MainFragment.kt
+++ app/src/main/java/com/ejemplos/dam/dosfragments/MainFragment.kt
@@ -5,11 +5,13 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+// para poder acceder a los elementos del layout
 import kotlinx.android.synthetic.main.fragment_main.*
+// libreria Anko para facilitar (aún mas) código
 import org.jetbrains.anko.support.v4.toast
 
 /**
- * A simple [Fragment] subclass.
+ * Fragment que vamos incluir en la Activity principal [Fragment subclass]
  */
 class MainFragment : Fragment() {
 
@@ -17,15 +19,27 @@ class MainFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    /**
+     * Cargamos el layout del fragment
+     */
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_main, container)
     }
 
+    /**
+     * Una vez cargado el layout podemos utilizar sus elementos
+     */
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // añadimos funcionalidad al boton
         main_button.setOnClickListener {
+            // Mostramos una toast
+            // Para utilizar este método necesitamos la librería Anko que completa las funcionalidades de Kotlin
+            // https://github.com/Kotlin/anko
+            // es necesario
             toast("Mensaje: ${main_editText.text}")
+            // mostramos en el TextView lo que introducimos en el EditText
             main_textview.text = main_editText.text
         }
     }
