065e259b26814bd777fa8a47bc279698401beaa4
Damian <damian@danielcastelao.org>
2018-01-31 07:47:07 +0100

    Activity principal y su layout

--- app/src/main/java/com/ejemplos/dam/dosfragments/MainActivity.kt
+++ app/src/main/java/com/ejemplos/dam/dosfragments/MainActivity.kt
@@ -3,6 +3,10 @@ package com.ejemplos.dam.dosfragments
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Clase creada para trabajar con Kotlin
+ * utiliza el layout activity_main que contiene al fragment referenciado con la clase MainFragment
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
